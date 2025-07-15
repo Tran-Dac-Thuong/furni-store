560ddc68aba19f4042d84fc7eb56b416ecd82f72
Tran-Dac-Thuong <hoangdeptraibodoiqua4321@gmail.com>
2025-07-15 15:36:12 +0700

    django-project

--- furni/settings.py
+++ furni/settings.py
@@ -119,12 +119,12 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'furni',
-        'USER': 'postgres',
-        'PASSWORD': 'thuong',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
